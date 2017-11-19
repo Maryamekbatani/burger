4d42f67904ca3421263d733e78d3fa7c328294c5
Maryam EKBATANI <maryamekbatani@Maryams-MacBook-Pro.local>
2017-11-19 14:48:22 -0500

    new files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
